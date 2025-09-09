3d0444203cfbff9cec94b5f131e26ac4b5794a45
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-09-09 14:18:11 +0100

    Add week 2 Python list examples
    Introduces a new file with examples demonstrating list indexing, element assignment, and appending in Python for week 2 class demos and solutions.

---
+++ Class demos and solutions/week2.py
@@ -0,0 +1,14 @@
+# Python examples: lists
+
+lottery = [15, 22, 36, 79]
+print(lottery[2])
+
+# Expect "36"
+
+lecturers = ["Aurelien", "Arthur", "Jim"]
+lecturers[1] = "Mark"
+
+print(lecturers)
+
+lecturers.append("Lorna")
+print(lecturers)
