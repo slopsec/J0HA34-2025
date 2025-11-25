f11ff7299eedd26dd157e298f8c6f4028c9a9b56
Aurelien Ammeloot <aurelienammeloot@fife.ac.uk>
2025-11-25 13:35:38 +0000

    Implement bubble sort for length-based token sorting
    Replaced the placeholder in len_sort with a bubble sort algorithm to sort tokens by length. Also added a call to len_sort in main for demonstration purposes.

--- Mini-Projects/miniproject1_sort.py
+++ Mini-Projects/miniproject1_sort.py
@@ -10,7 +10,30 @@ def display_tokens(token_array):
 # len sort
 # receive an array of tokens
 def len_sort(token_array):
-    pass
+
+    # # Pro: Optimised - for list size
+    # # Con: Algorithm unknown
+    # token_array = sorted(token_array, key=len)
+
+    # display_tokens(token_array)
+
+    # Implementing a bubble sort
+    # No adaptation needed for type
+    length = len(token_array)
+
+    # Outer loop
+    for i in range(length - 1):
+        sorted = True
+        # Inner loop -> doing the swaps
+        for j in range(length - 1 - i):
+            # If necessary, swap and flag as unsorted
+            if len(token_array[j]) > len(token_array[j+1]):
+                token_array[j], token_array[j+1] = token_array[j+1], token_array[j]
+                sorted = False
+        
+        if sorted: break
+
+    display_tokens(token_array)
 
 # Alphabetical sort
 # receive an array of tokens
@@ -61,6 +84,8 @@ def main():
     main_text = input_validate()
     token_list = tokenise(main_text)
     sort_alphabet(token_list)
+
+    len_sort(token_list)
 
     # # Just for testing
     # print(token_list)
