a6566d5c8dea5c315cdc1d92fe805020536dd8aa
aurelienfife <161948862+aurelienfife@users.noreply.github.com>
2025-12-16 14:52:57 +0000

    Implement playable Rock Paper Scissors game logic
    Added random computer choice, score tracking, and replay loop to the Rock Paper Scissors game. The winner is now determined using modulus arithmetic for efficiency, and user input is handled in a loop until the player chooses to stop.

--- Mini-Projects/miniproject2_rps.py
+++ Mini-Projects/miniproject2_rps.py
@@ -1,13 +1,32 @@
+import random
 # Basic rock paper scissors
 
 
 def determine_winner(user_choice):
-
+    ''' By using modulus arithmetic, we can reduce the number of test
+    cases to three and treat the choices as mere numbers. (1,2,3)
+    In contrast, using match/case or if/else would result in 9 cases (5 with optimisations)
+    Or creating objects/dictionaries for each move (with the winning/losing moves) requires
+    extra coding.
     '''
-    How are we going to solve this?
-    '''
+    computer_choice = random.randint(1,3)
 
-    pass
+    choices = {
+        1: "Rock",
+        2: "Paper",
+        3: "Scissors" }
+    # Temp
+    print("Computer choice:", choices[computer_choice])
+
+    if user_choice == computer_choice:    # Draw
+        return 0
+    elif (user_choice+1)%3 == computer_choice%3:
+        # We shift user choice by and divide by 3
+        # The remainders match when computer wins
+        return 1 # For computer
+    else:
+        return -1 # For user    
+
 
 def get_input():
     print("Welcome to R,P,S")
@@ -27,8 +46,31 @@ def main():
     user_score = 0
     computer_score = 0
 
-    # Get input
-    user_move = get_input()
+    replay = True
+
+    while replay is True:
+        # Get input
+        user_move = get_input()
+        winner = determine_winner(user_move)
+
+        # determine output
+        if winner == 0:
+            print("Draw")
+        elif winner == 1:
+            print("Computer win.")
+            computer_score += 1
+        else:
+            print("User win.")
+            user_score +=1
+
+        print("User:", user_score, " --- Computer:", computer_score)
+
+        wish = input("Play again? (y/n)")
+
+        if wish[0].lower() == "y":
+            replay == True
+        else:
+            replay == False
 
 
 
